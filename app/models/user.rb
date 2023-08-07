babf1dc667d53b59f0033725e70967bf3f776e2e
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-07 15:29:22 +0900

    ユーザーモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'must be full-width characters' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'must be full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be full-width katakana characters' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'must be full-width katakana characters' }
   validates :birth_date, presence: true
+  validate :password_complexity
+
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    unless password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)/)
+      errors.add :password, 'must include both letters and numbers'
+    end
+  end
+
 end
