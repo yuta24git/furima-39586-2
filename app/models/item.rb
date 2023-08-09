42fd9c5f339279aafd79bfa5dc8ad1b6e203f823
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-09 14:40:18 +0900

    アクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-validates :title, persence :true
-validates :explanation, persence :true
-validates :category_id, persence :true
-validates :situation_id, persence :true
-validates :responsibility_id, persence :true
-validates :prefecture_id ,persence :true
-validates :number_of_day_id, persence :true
-validates :price, persence :true
+  validates :title, persence :true
+  validates :explanation, persence :true
+  validates :category_id, persence :true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :situation_id, persence :true
+  validates :responsibility_id, persence :true
+  validates :prefecture_id ,persence :true
+  validates :number_of_day_id, persence :true
+  validates :price, persence :true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
--- db/migrate/20230809014216_create_items.rb
+++ db/migrate/20230809014216_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :title,     null: false
+      t.string :title, null: false
       t.text :explanation, null: false
       t.integer :category_id, null: false
       t.integer :situation_id, null: false
