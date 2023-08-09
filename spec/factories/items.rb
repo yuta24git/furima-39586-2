da2a9f7bbb0add6b721a05579ddf9b2a35504bfb
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-09 20:14:13 +0900

    商品出品機能のテストコードを実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title { Faker::Lorem.word }
+    explanation { Faker::Lorem.sentence }
+    category_id { rand( 2 .. 11 ) }
+    situation_id { rand( 2 .. 7 ) }
+    responsibility_id { rand( 2 .. 3 ) }
+    prefecture_id { rand( 2 .. 48 ) }
+    number_of_day_id { rand( 2 .. 3 ) }
+    price { rand( 300 .. 9999999 ) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
