f0296b076042e0cc8313035cc090bddfe0962df3
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-11 11:59:58 +0900

    コントローラーにて情報を保存できるよう設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,20 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @buyer = Buyer.create(buyer_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+  def order_params
+    params.permit(buyer_id: current_user.id).merge(user_id: @item.user_id)
+  end
+  
+  def address_params
+    params.permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number ).mergr(buyer_id: @buyer.id)
   end
 end
