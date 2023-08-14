92fa6f320c3f0a69e0a8a8c98c911594e88eb927
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-14 22:25:00 +0900

    BuyerAddressクラスの修正

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,7 +1,7 @@
 class BuyerAddress
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number, :user_id, :item_id,
-                :price, :token
+                :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
@@ -11,7 +11,6 @@ class BuyerAddress
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :user_id
     validates :item_id
-    validates :price
     validates :token
   end
 
