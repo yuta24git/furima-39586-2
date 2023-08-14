47411665340cfca8d44d1c5b311aaedaf1f2d58e
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-14 22:23:04 +0900

    orders_controllerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,16 @@
 class OrdersController < ApplicationController
   include ItemsHelper
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_page, only: :index
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-    @item = Item.find(params[:item_id])
+    set_item
     @buyer_address = BuyerAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
+    set_item
     @buyer_address = BuyerAddress.new(order_params)
     if @buyer_address.valid?
       pay_item
@@ -24,25 +25,27 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:buyer_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name,
-                                          :telephone_number, buyer_id: current_user.id).merge(user_id: @item.user_id, item_id: @item.id,
-                                                                                              price: @item.price, token: params[:token])
+                                          :telephone_number).merge(user_id: @item.user_id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: order_params[:price],
+      amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
     )
   end
 
   def move_to_page
-    @item = Item.find(params[:item_id])
     if !user_signed_in?
       redirect_to new_user_session_path
     elsif @item.user_id == current_user.id || buyers_exists?(@item.id)
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
