31e604a2b3d7d1e47047ad4196664f2c70adc3c8
yuta24git <whs.soccer.24.0803@gmail.com>
2023-08-17 19:14:50 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'furima-39586-2'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yuta24git/furima-39586-2.git'
-set :branch, 'main'
+set :branch, 'master'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
